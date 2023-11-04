fe6c4411cc9ffeded06c1b46407617b47558f05c
palm <tauman.exe@gmail.com>
2023-11-04 16:10:16 +0100

    Add Project

---
+++ AutoTiles/scripts/AutoTileTemplate.gd
@@ -0,0 +1,15 @@
+@tool
+extends Node2D
+
+@export var ReadFrom:TileMap
+@export var Paint: Array[TileMap] = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
